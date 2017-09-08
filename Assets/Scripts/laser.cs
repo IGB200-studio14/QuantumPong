ce817e8fcb8871b156bbc0651439ae579c8a2c35
rino776 <rinoharrold@gmail.com>
2017-09-08 23:24:24 +1000

    player can shoot at 'battery'
    the lasers now move to the edge of the screen, and disapear when it reaches the edge, updating a counter each time a laser reaches the end.

--- Assets/Scripts/laser.cs
+++ Assets/Scripts/laser.cs
@@ -6,6 +6,7 @@ public class laser : MonoBehaviour {
 
 	public float speed = 10;
 
+
 	// Use this for initialization
 	void Start () {
 		
@@ -14,6 +15,18 @@ public class laser : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		move();
+		batteryCollision();
+	}
+
+	void batteryCollision() {
+		//if the laser goes past the battery on the right hand of the screen
+		if (this.transform.position.x >= 12) {
+
+			//update the batteries charge
+			GameObject.Find("GameManager").GetComponent<GameManager>().playerOneBattery += 1;
+
+			Destroy(this.gameObject);
+		}
 	}
 
 	void move() {
