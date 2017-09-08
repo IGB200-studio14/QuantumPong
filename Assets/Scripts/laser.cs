2e910bb2d6e8f980e3d8fb12a541d96a40cf07eb
rino776 <rinoharrold@gmail.com>
2017-09-08 23:03:16 +1000

    lasers move
    lasers now move to the right of the screen.

---
+++ Assets/Scripts/laser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laser : MonoBehaviour {
+
+	public float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		move();
+	}
+
+	void move() {
+		Vector3 position = this.transform.position;
+
+		position.x += speed * Time.deltaTime;
+
+		this.transform.position = position;
+	}
+}
