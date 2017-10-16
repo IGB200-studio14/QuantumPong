76b18c3ac2c1fc68c88ceebb347948b541fc3351
rino776 <ryan.harrold@connect.qut.edu.au>
2017-10-16 23:28:40 +1000

    Added 'regen' powerup
    added a power up that reduces the charge of the battery

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,37 +19,61 @@ public class GameManager : MonoBehaviour {
 
 	public float rechargeRate = 0.1f;
 
+	public GameObject powerUp;
+
+	//frames since the game started
+	private int frames;
+
+	//how many seconds till we spawn a powerup
+	public int powerupTime = (int)(0.25 * 60);
+
 	// Use this for initialization
 	void Start () {
-
+		
 	}
 
 	// Update is called once per frame
 	void Update () {
+		frames++;
 
 		playerOneSlider.value = playerOneBattery;
 		playerTwoSlider.value = playerTwoBattery;
 
-		if(playerOneCharge < 10){
+		playerCharge();
+
+		powerUps();
+	}
+
+	void powerUps() {
+		if (frames % (powerupTime * 60) == 0) {
+			GameObject temp = Instantiate(powerUp, new Vector3(0,0,0), this.transform.rotation);
+			//TODO get this to pick randomly from all possible powerups
+			temp.GetComponent<PowerUp>().id = 1;
+		}
+		
+	}
+
+	void playerCharge() {
+		if (playerOneCharge < 10) {
 			//just guessing this value
 			playerOneCharge += rechargeRate;
 		}
 
-		if(playerTwoCharge < 10){
+		if (playerTwoCharge < 10) {
 			playerTwoCharge += rechargeRate;
 		}
 
-		if(playerOneCharge < -1){
+		if (playerOneCharge < -1) {
 			playerOneCharge = 0;
 		}
 
 
-		if(playerTwoCharge < -1){
+		if (playerTwoCharge < -1) {
 			playerTwoCharge = 0;
 		}
 
 		playerOneChargeSlider.value = playerOneCharge;
 		playerTwoChargeSlider.value = playerTwoCharge;
-
 	}
+
 }
---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour {
+
+	public int id = 0;
+	//should be only one(left, p1) or -1(right, p2)
+	public int player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.localScale = new Vector3(10,10,0);
+	}
+
+	void reduceBatteryCharge() {
+		Debug.Log("player: " + player);
+		if (player == -1) {
+			GameObject.Find("GameManager").GetComponent<GameManager>().playerOneBattery -= 5;
+		}else if (player == 1) {
+			GameObject.Find("GameManager").GetComponent<GameManager>().playerTwoBattery -= 5;
+		}
+	}
+
+	public void OnCollisionEnter2D(Collision2D col) {
+		//only do stuff if we collide with a laser
+		if (col.gameObject.tag == "laser") {
+
+			player = (int)(col.gameObject.GetComponent<laser>().speed / 10);
+
+			//do different things according to it's id
+			switch (id) {
+				//reduce damage taken/charge given
+				case 1:
+					reduceBatteryCharge();
+					break;
+
+				default:
+					break;
+			}
+
+			Destroy(col.gameObject);
+			Destroy(this.gameObject);
+		}
+	}
+}
