dc7ddc9e3edc2b9dbbb597ac6854cdc46d80e819
DOD918 <thc918@qq.com>
2017-10-22 16:27:40 +1000

    "play" button works

---
+++ Assets/play.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class play : MonoBehaviour {
+    public void Game()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+	}
+
