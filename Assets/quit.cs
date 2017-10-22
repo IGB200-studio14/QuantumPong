80034a7be37e170fe94841d246a5c7a18ba86373
DOD918 <thc918@qq.com>
2017-10-22 16:14:59 +1000

    'quit' function works

---
+++ Assets/quit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quit : MonoBehaviour {
+
+	public void doquit()
+    {
+        Debug.Log("has quit game");
+        Application.Quit();
+    }
+}
